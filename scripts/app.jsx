e35c5eaf5dc23a34c8e021ad1f4cc1e8978c8dcf
Matt Lee <matt@reddit.com>
2014-11-03 22:04:07 -0800

    React class mutation demo.

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,8 +1,83 @@
 
+var Foo = React.createClass({
+  render: function() {
+    return ( <div className="foo">
+      <header>
+        <h1>This is a foo!</h1>
+      </header>
+      <Content>
+        A foo has a header and some content.
+      </Content>
+    </div> );
+  },
+});
+
+var Content = React.createClass({
+  render: function() {
+    return ( <div className="content" {...this.props} /> );
+  },
+});
+
+
+/**
+ * returns a React class with the given mutator functions applied
+ * @param  {React class} ElementClass a class define with React.createClass
+ * @param  {function[]} any number of functions to mutate the result of the 
+ *                          ElementClass's render function
+ * @return {React class}              
+ */
+function mutate(ElementClass /* ...mutators */) {
+  var mutators = Array.prototype.slice.call(arguments, 1);
+  return React.createClass({
+    render: function() {
+      var renderedElement = (
+        <ElementClass {...this.props} />
+      ).type.prototype.render();
+      
+      return mutators.forEach(function(element, mutator) {
+          mutator.call(element);  
+          return element;
+        }, 
+        renderedElement  
+      );
+    },
+  });
+}
+
+/**
+ * add a footer to a react element
+ */
+var Bar = function() {
+  this.props.children.push( <footer>
+    The Bar mutator adds a footer in!
+  </footer> );
+};
+
+/**
+ * take each Content child, wrap its current children in a <del> tag, then
+ * add a <div.new-content> element with some new content.
+ */
+var Baz = function() {
+  this.props.children.filter(function(element) {    
+    return element.type.displayName === 'Content';
+  }).forEach(function(element) {
+    element.props.children = [
+      ( <del>{ element.props.children }</del> ),
+      ( <div class="new-content">
+        The Baz mutator replaces Content elements!
+      </div> )
+    ];
+  })
+};
+
+// replace the original <Foo> with a class mutated by Bar and Baz
+Foo = mutate(Foo, Bar, Baz);
+
+
 window.onReady(function() {
   var mountNode = document.getElementById('app');
   React.render(
-    ( <div>React is ready to go!</div> ),
+    ( <Foo /> ),
     mountNode
   );
 });
